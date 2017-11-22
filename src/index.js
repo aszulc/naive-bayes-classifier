616672eb18577b9f56df5c6d33e95e11f3e6d810
aszulc <aszulc1990@gmail.com>
2017-11-22 18:30:23 +0100

    Config #3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-for(let i = 0; i < 5; i++) {
-  setTimeout(() => console.info(i), 0);
+// @flow
+
+/**
+ * Calculates a square
+ * @param {number} n - number
+ */
+function square(n: number): number {
+  return n * n;
 }
+
+/**
+ * Start the program
+ */
+function main() {
+  for (let i = 0; i < 5; i += 1) {
+    setTimeout(() => alert(square(i)), 0);
+  }
+}
+
+main();
